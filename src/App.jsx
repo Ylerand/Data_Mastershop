8c7dfdf2f1f9a8e8b9c4aa9b24187d3ca845cb3d
Emmanuel <em.ylerand@duocuc.cl>
2026-02-03 02:21:07 -0300

    Fix: Corrigiendo imports en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import { ProductProvider } from './context/ProductContext' // <--- ¡AQUÍ ESTÁ
 // Importamos las páginas y componentes
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
 import Account from './pages/Account'
 import AdminLogin from './pages/AdminLogin'
-import Dashboard from './pages/Dashboard'
 import Footer from './components/Footer'
 import CartDrawer from './components/CartDrawer'
 import './App.css'
